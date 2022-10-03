8065ca3b2e67f55fbe43aea3b6dc04b0004233e1
MrMarvel <seregakkk999@yandex.ru>
2022-10-03 05:55:42 +0300

    added tests and dependencies

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,36 @@
+import org.fest.swing.fixture.FrameFixture;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+class GuiMainTest {
+    private FrameFixture app;
+
+    @BeforeEach
+    public void setUp() {
+        app = new FrameFixture(new Main());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        app.cleanUp();
+    }
+    @Test
+    void testStartUp() throws InterruptedException {
+        Thread.sleep(1);
+    }
+}
+
+class FileMainTest {
+    private String filenameOut;
+    @AfterEach
+    public void clearFiles() {
+        try {
+            Files.delete(Path.of(filenameOut));
+        } catch (IOException ignored) {}
+    }
+}
